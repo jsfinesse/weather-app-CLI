763b534940ae7ef3380132b317cb5fdbc8cef918
Prithu Singh <prithusingh0327@gmail.com>
2022-03-18 15:08:52 +0530

    Added geocoding api and error handling

--- app.js
+++ app.js
@@ -15,12 +15,18 @@ request({ url: url, json: true }, (error, response) => {
     }
 });
 
-// const geocodeURL =
-//     "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoianNmaW5lc3NlIiwiYSI6ImNsMHc2bnpvcjFlNnYzanFvZXpjemNmc3QifQ.e-59IbJarPkeqESCWTD1Nw&limit=1";
+const geocodeURL =
+    "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoianNmaW5lc3NlIiwiYSI6ImNsMHc2bnpvcjFlNnYzanFvZXpjemNmc3QifQ.e-59IbJarPkeqESCWTD1Nw&limit=1";
 
-// request({ url: geocodeURL, json: true }, (error, response) => {
-//     const latitude = response.body.features[0].center[1];
-//     const longitude = response.body.features[0].center[0];
+request({ url: geocodeURL, json: true }, (error, response) => {
+    if (error) {
+        console.log("Can't reach the servers!");
+    } else if (response.body.features.length === 0) {
+        console.log("Specified location is not in our database");
+    } else {
+        const latitude = response.body.features[0].center[1];
+        const longitude = response.body.features[0].center[0];
 
-//     console.log(latitude, longitude);
-// });
+        console.log(latitude, longitude);
+    }
+});
